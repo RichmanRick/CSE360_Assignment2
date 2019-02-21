13d12cdbf66f954b9849dc511601f748d8ba5eaf
RichmanRick <richardsc210@gmail.com>
2019-02-21 03:20:45 -0700

    Initial commit for cse360

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,51 @@
+package cse360assign2;
+
+/**
+ * Calculator class does...
+ * 
+ * @author Richard Cao <br>
+ * ClassID: 342 <br>
+ * Assignment #2
+ * @version %I%, %G%
+ *
+ */
+public class Calculator 
+{
+
+	private int total;
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	public void divide (int value) 
+	{
+		
+	}
+	
+	public String getHistory () 
+	{
+		return "";
+	}
+}
